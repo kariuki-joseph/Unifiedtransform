2996d6f9361be6b26ba40913afdff6de0fcc032c
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-06-22 11:41:31 +0300

    make address nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('gender');
             $table->string('nationality');
             $table->string('phone');
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->string('county');
             $table->string('town');
             $table->string('photo')->nullable();
